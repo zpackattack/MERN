ecbb4ac943f862dc3e4ea610b86027e677815947
zpackattack <Treyz3@icloud.com>
2022-10-10 00:45:42 -0400

    tg
    v

---
+++ MERN Stack/mern-emsystem/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          We did it!!!<br></br>
+          MERN Stack
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>could be useful later</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
